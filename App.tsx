dc346cdddbfc727ffbc0c39b3410ce941c2d5bd9
mayankpatel1004 <mayank.patel104@gmail.com>
2026-05-23 14:02:48 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import { StatusBar, StyleSheet, View, Text } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="white"
+      />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          paddingTop: safeAreaInsets.top,
+          paddingBottom: safeAreaInsets.bottom,
+        },
+      ]}>
+      <Text style={styles.text}>
+        Hello RN 0.85
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
+
+export default App;
